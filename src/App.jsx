e05f6a70bbb66c09dce92d34cf1c12b092f79d64
unknown <septiyanrivandi@gmail.com>
2024-05-01 13:17:59 +0700

    ubah teks di app

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { productInputs, userInputs } from "./formsource"
 
 function App() {
   return (
@@ -17,12 +18,18 @@ function App() {
             <Route path="users">
               <Route index element={<List />}></Route>
               <Route path=":userId" element={<Single />}></Route>
-              <Route path="new" element={<New />}></Route>
+              <Route 
+                path="new" 
+                element={<New inputs={userInputs} title="Add New User"/>}
+                />
             </Route>
             <Route path="products">
               <Route index element={<List />}></Route>
               <Route path=":productsId" element={<Single />}></Route>
-              <Route path="new" element={<New />}></Route>
+              <Route
+                path="new" 
+                element={<New inputs={productInputs} title="Add New Products"/>}
+                />
             </Route>
           </Route>
         </Routes>
