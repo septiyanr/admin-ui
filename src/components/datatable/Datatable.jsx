be2614c9b90f54e8337c6722650ad47ee4dbd16f
unknown <septiyanrivandi@gmail.com>
2024-06-13 08:40:28 +0700

    ubah teks di app

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,6 +1,6 @@
 import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const columns = [
   { field: "id", headerName: "ID", width: 70 },
@@ -34,6 +34,10 @@ const rows = [
   { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
 ];
 
+const Datatable = () => {
+  const location = useLocation();
+  const type = location.pathname.split('/')[1];
+
 const actionColumn = [
   {
     field: "action",
@@ -51,12 +55,11 @@ const actionColumn = [
   },
 ];
 
-const Datatable = () => {
   return (
     <div className="datatable">
       <div className="datatableTitle">
-        Users
-        <Link to="/users/new" className="link">
+        {type.toUpperCase()}
+        <Link to={"/" + type + "/new"} className="link">
           Add New
         </Link>
       </div>
